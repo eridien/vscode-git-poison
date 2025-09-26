25e4bd898305691dc58797a8a2ab295129716a8a
Mark Hahn <mark@hahnca.com>
2025-09-26 12:52:38 -0700

    finished readme

--- src/config.ts
+++ src/config.ts
@@ -28,5 +28,6 @@ export function getShowStatusBar(): boolean {
 export function getExcludeGlobs(): string {
   return vscode.workspace
                .getConfiguration(EXT_ID).get<string>('excludeGlobs') ?? 
-          '**/{.git,node_modules,dist,build,.cache,out,tmp,temp,coverage}/**';
+          "**&#47;{.git,node_modules,dist,build,.cache,out,tmp,temp,coverage}&#47;**
+";
 }
